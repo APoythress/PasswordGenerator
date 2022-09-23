b692953e28b5464569404119ee99e664ed3b05c1
Austin Poythress <poythress213@gmail.com>
2022-09-23 18:03:00 -0400

    Issue printing values to web page
    Need to figure out a way to grab the values from the model and have it print to the html dynamically each time the update button is pressed.

---
+++ PasswordGenerator/Controllers/PasswordsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using PasswordGeneratorClasses;
+using PasswordGeneratorClasses.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PasswordGenerator.Controllers
+{
+    public class PasswordsController : Controller
+    {
+        public ActionResult Index()
+        {
+            PasswordsViewModel model = new();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(int numOfChars, bool useSpecial)
+        {
+            PasswordsViewModel model = new();
+            PasswordController.GetPassword();
+            return View(model);
+        }
+
+        public IActionResult Update()
+        {
+            PasswordsViewModel model = new PasswordsViewModel();
+            return View(model);
+        }
+    }
+}
